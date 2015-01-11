5bc9ff3a8cec6d0a75b8abcc067f464e5196b05b
tbonelaforge <ttf0809@gmail.com>
2015-01-11 10:57:46 -0800

    Changing to use 'price is right' binary search condition:
    Greatest index 'i' such that value(i) <= target. (Big as you can without going over).

--- binary_search.c
+++ binary_search.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
 
 #include "binary_search.h"
 #include "sort_util.h"
 
+/* Return greatest index i such that value(i) <= target,
+   Or -1, if value(i) > target for all i */
 int binary_search(struct sortable * self, int begin, int end, void * target) {
     int m;
     int comparison;
@@ -11,7 +14,7 @@ int binary_search(struct sortable * self, int begin, int end, void * target) {
     }
     m = pick_midpoint(begin, end);
     comparison = (*self->comp)(self->elements[m], target);
-    if (comparison < 0) {
+    if (comparison <= 0) {
         return binary_search(self, m, end, target);
     } else { // comparison >= 0
         return binary_search(self, begin, m, target);
@@ -24,13 +27,12 @@ int determine_final_index(struct sortable * self, int begin, int end, void * tar
     int compare_to_begin = (*self->comp)(target, begin_element);
     int compare_to_end = (*self->comp)(target, end_element);
 
-    if (compare_to_begin <= 0) { // No elements less than targets.
-        return -1;
+    if (compare_to_end >= 0) {
+        return end;
     }
-    if (compare_to_end <= 0) { // target between beginning and ending index.
-        return begin; // Greatest index i having value(i) < target.
+    //    return begin;
+    if (compare_to_begin >= 0) {
+        return begin;
     }
-
-    // Otherwise, we have target > ending index
-    return end;
+    return -1;
 }
